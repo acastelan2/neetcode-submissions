43add9fc470f06f9ce8d84911817beede1ea0475
acastelan2 <109180396+acastelan2@users.noreply.github.com>
2026-06-15 17:40:32 -0500

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+        var res = new int[n];
+        Array.Fill(res, 1);
+
+        for (int i = 1; i < n; i++){
+            res[i] = nums[i-1] * res[i-1];
+        }
+
+        int suffix = 1;
+        for (int i = n-1; i >= 0; i--){
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+}
