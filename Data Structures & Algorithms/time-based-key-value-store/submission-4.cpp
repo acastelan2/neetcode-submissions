8cf13e29918408659009a1d5e5851a1b7d804938
acastelan2 <109180396+acastelan2@users.noreply.github.com>
2026-07-07 20:50:47 -0500

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.cpp
@@ -0,0 +1,32 @@
+struct TsPair {
+    string value;
+    int timestamp;
+};
+
+class TimeMap {
+public:
+    unordered_map<string, vector<TsPair>> uMap;
+    TimeMap(){}
+    
+    void set(string key, string value, int timestamp) {
+        uMap[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        const auto& vec = uMap[key];
+        int low = 0, high = vec.size()-1;
+        string res = "";
+        
+        while (low <= high){
+            int mid = low + (high-low) / 2;
+            if (vec[mid].timestamp == timestamp) return vec[mid].value;
+            else if (vec[mid].timestamp < timestamp){
+                res = vec[mid].value; 
+                low = mid + 1;   
+            }
+            else high = mid - 1;
+        }
+
+        return res;
+    }
+};
